27c165d150953fc18887f8b1ce27256c3f915200
wzc520pyfm <1528857653@qq.com>
2023-04-12 14:17:02 +0800

    feat(component): add http client in cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -11,8 +12,10 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
+  // 全局注册
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     // 路由声明
     RouterModule.forRoot([
