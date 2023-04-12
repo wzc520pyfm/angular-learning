deac420242088eed522fb65025dc4256d095420c
wzc520pyfm <1528857653@qq.com>
2023-04-12 11:51:33 +0800

    feat(app): update product details component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  
+  product: Product | undefined;
 
+  // 注入此组件的ActivatedRoute, 用于访问路由信息
+  constructor(private route: ActivatedRoute) { }
+  
+  ngOnInit(): void {
+    // 从路由参数中获取productId
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // 在产品列表中查找productId的产品
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
 }
